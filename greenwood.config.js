78470679b005b70fc582129e6dd79799ca282462
Owen Buckley <owenbuckley13@gmail.com>
2021-10-24 13:36:05 -0400

    fix title

--- greenwood.config.js
+++ greenwood.config.js
@@ -2,7 +2,7 @@ const pluginImportCss = require('@greenwood/plugin-import-css');
 const pluginThemePresentation = require('greenwood-starter-presentation');
 
 module.exports = {
-  title: 'Find Your GitHub Flow',
+  title: 'Finding Your GitHub Flow',
   
   plugins: [
     ...pluginImportCss(),
